2e8518f254094c059d03d9b45ef66ab759933e17
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-12-02 17:53:38 +0100

    Created a main panel with sliders to make RGB colors.

---
+++ WindowsLEDApp/WindowsLEDApp/MainPanel.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsLEDApp
+{
+    partial class MainPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RedTrackBar = new System.Windows.Forms.TrackBar();
+            this.GreenTrackBar = new System.Windows.Forms.TrackBar();
+            this.BlueTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RedTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RedTrackBar
+            // 
+            this.RedTrackBar.Location = new System.Drawing.Point(170, 13);
+            this.RedTrackBar.Name = "RedTrackBar";
+            this.RedTrackBar.Size = new System.Drawing.Size(790, 80);
+            this.RedTrackBar.TabIndex = 0;
+            // 
+            // GreenTrackBar
+            // 
+            this.GreenTrackBar.Location = new System.Drawing.Point(170, 75);
+            this.GreenTrackBar.Name = "GreenTrackBar";
+            this.GreenTrackBar.Size = new System.Drawing.Size(790, 80);
+            this.GreenTrackBar.TabIndex = 1;
+            // 
+            // BlueTrackBar
+            // 
+            this.BlueTrackBar.Location = new System.Drawing.Point(170, 135);
+            this.BlueTrackBar.Name = "BlueTrackBar";
+            this.BlueTrackBar.Size = new System.Drawing.Size(790, 80);
+            this.BlueTrackBar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Red:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Green:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Blue:";
+            // 
+            // MainPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 549);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BlueTrackBar);
+            this.Controls.Add(this.GreenTrackBar);
+            this.Controls.Add(this.RedTrackBar);
+            this.Name = "MainPanel";
+            this.Text = "MainPanel";
+            ((System.ComponentModel.ISupportInitialize)(this.RedTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar RedTrackBar;
+        private System.Windows.Forms.TrackBar GreenTrackBar;
+        private System.Windows.Forms.TrackBar BlueTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
